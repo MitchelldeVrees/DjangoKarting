945765f319e1fd1140173b77f89cb214e4d98e90
Mitchell de Vries <mitchell.de.vries3@hva.nl>
2021-09-06 20:29:11 +0200

    password reset workss

--- django/web_project/settings.py
+++ django/web_project/settings.py
@@ -137,3 +137,10 @@ STATICFILES_DIRS = (
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+#SMTP
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST ="smtp.gmail.com"
+EMAIL_PORT=587
+EMAIL_USE_TLS= True
+EMAIL_HOST_USER="mitchelldvrees@gmail.com"
+EMAIL_HOST_PASSWORD="#Vrees#Vrees"
